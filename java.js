08685ce1bb310d3b06b4acd00ee203f650fd289a
l0fi-coding <codinglofi3@gmail.com>
2024-04-09 07:47:53 -0400

    added the etch feature

--- java.js
+++ java.js
@@ -1,30 +1,39 @@
 const container= document.querySelector(".container");
-function defaultgrid(){
-    for (let i = 0; i < 256; i++) {
-        
-        const square = document.createElement("div");
-        container.appendChild(square);
-      }
+
       
-}
 
 function createGrid(){
     let num=0
+
+   
+
     for ( i= 0; i < 16; i++) {
-        
         const square = document.createElement("div");
         square.classList.add("row")
         container.appendChild(square);
         for (let i = 0; i < 16; i++) {
-            
             const block = document.createElement("div");
             block.classList.add("column")
             square.appendChild(block);
+            block.addEventListener("mouseover", (event) => {
+                block.style.background="black"
+              });
       }
+    
     }
-  
-}
-createGrid()
+    
+};
 
+createGrid()
+let change=querySelector(".column")
+change.addEventListener('mouseleave', (event) => {
+    
+   
+       change.style.background="black"
+    
+
+
+    
+    })
 
 
